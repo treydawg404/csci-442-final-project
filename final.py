9e0e5aed268b78551314df993a3ee112d86fb5e5
Trey <treyton.exe@gmail.com>
2023-05-02 17:57:10 -0600

    other method

--- final.py
+++ final.py
@@ -90,9 +90,9 @@ def orientation_cone():
             hsv = cv2.cvtColor(color_image, cv2.COLOR_BGR2HSV)
             orange_lower = np.array([0, 250, 50], np.uint8)
             orange_upper = np.array([60, 255, 255], np.uint8)
+            hsv = cv2.dilate(hsv,kernel)
             orange_mask = cv2.inRange(hsv, orange_lower, orange_upper)
 
-            orange_mask = cv2.dilate(orange_mask, kernel)
 
             Moments = cv2.moments(orange_mask)
             if Moments["m00"] != 0:

15907f2adcd07177c769b63fbfee20d0e6a9f399
Trey <treyton.exe@gmail.com>
2023-05-01 21:26:27 -0600

    ""

--- final.py
+++ final.py
@@ -119,29 +119,7 @@ def oreitnation_cone():
                 body = 0
                 tango.setTarget(MOTORS, motors)
                 tango.setTarget(BODY, body)
-            else:
-                if (cX > 370):
-                    motors -= 200
-                    if(motors < 5000):
-                        motors = 5000
-                        tango.setTarget(MOTORS, motors)
-                elif (cX < 270):
-                    motors += 200
-                    if(motors > 7000):
-                        motors = 7000
-                        tango.setTarget(MOTORS, motors)
-                else:
-                    motors = 6000
-                    tango.setTarget(MOTORS, motors)
-
-                if(distance > 1):
-                    motors = 6000
-                    tango.setTarget(MOTORS,motors)
-                    body = 5200            
-                    tango.setTarget(BODY,body)
-                else:
-                    body = 6000
-                    tango.setTarget(BODY,body)
+            
 
             print(cX)
             print(cY)

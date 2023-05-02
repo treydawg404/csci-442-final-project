b05bbd1006cb123fc3819e6a3944d89bba473e70
Trey <treyton.exe@gmail.com>
2023-05-02 15:31:56 -0600

    ""

--- final.py
+++ final.py
@@ -107,9 +107,9 @@ def orientation_cone():
 
             #print((cv2.countNonZero(orange_mask) / orange_mask.size))
             if (((cv2.countNonZero(orange_mask) / orange_mask.size) < 0.001) or ((cv2.countNonZero(orange_mask) / orange_mask.size) > 0.5)):
-                motors += 200
-                if(motors > 7200):
-                    motors = 7200
+                motors += 500
+                if(motors > 7000):
+                    motors = 7000
                     tango.setTarget(MOTORS, motors)
 
             else:

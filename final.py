59b2dcb9fbf476a9f29c17f2a6283c4b7d21cdd1
Trey <treyton.exe@gmail.com>
2023-05-01 20:22:09 -0600

    ""

--- final.py
+++ final.py
@@ -32,16 +32,6 @@ def orient_finish():
         color_image = np.asanyarray(color_frame.get_data())
         depth_image = np.asanyarray(depth_frame.get_data())
 
-        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
-
-        depth_colormap_dim = depth_colormap.shape
-        color_colormap_dim = color_image.shape
-
-        if depth_colormap_dim != color_colormap_dim:
-            resized_color_image = cv2.resize(color_image, dsize=(depth_colormap_dim[1], depth_colormap_dim[0]), interpolation=cv2.INTER_AREA)
-            images = np.hstack((resized_color_image, depth_colormap))
-        else:
-            images = np.hstack((color_image, depth_colormap))
         hsv = cv2.cvtColor(color_image, cv2.COLOR_BGR2HSV)
 
         if(inMiningArea == False):
@@ -56,7 +46,7 @@ def orient_finish():
                 cX, cY = 0,0
             cv2.circle(color_image, (cX, cY), 5, (0, 165, 255), -1)
 
-            distance = images.get_distance(cX,cY)
+            distance = depth_image.get_distance(cX,cY)
 
         cv2.namedWindow('RobotVision', cv2.WINDOW_AUTOSIZE)
         cv2.imshow('RobotVision', color_image) 

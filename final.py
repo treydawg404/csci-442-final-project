bc49754e0c6c3a12a048131a317c92f4496870f7
Trey <treyton.exe@gmail.com>
2023-05-01 20:49:55 -0600

    shot in the dark

--- final.py
+++ final.py
@@ -11,114 +11,131 @@ import pyrealsense2 as rs
 import numpy as np
 import cv2
 import time
-from maestro import Controller
+import math
+from maestro import Controller                                                     
 
-def orient_finish():
-    MOTORS = 1
-    TURN = 2
-    BODY = 0
+MOTORS = 1
+TURN = 2
+BODY = 0
 
-    tango = Controller()
-    motors = 6000
-    turns = 6000
-    body = 6000
+tango = Controller()
+motors = 6000
+turns = 6000
+body = 6000
 
-    # Configure depth and color streams
-    pipeline = rs.pipeline()
-    config = rs.config()
+# Configure depth and color streams
+pipeline = rs.pipeline()
+config = rs.config()
 
-    # Get device product line for setting a supporting resolution
-    pipeline_wrapper = rs.pipeline_wrapper(pipeline)
-    pipeline_profile = config.resolve(pipeline_wrapper)
-    device = pipeline_profile.get_device()
-    device_product_line = str(device.get_info(rs.camera_info.product_line))
+# Get device product line for setting a supporting resolution
+pipeline_wrapper = rs.pipeline_wrapper(pipeline)
+pipeline_profile = config.resolve(pipeline_wrapper)
+device = pipeline_profile.get_device()
+device_product_line = str(device.get_info(rs.camera_info.product_line))
 
-    found_rgb = False
-    for s in device.sensors:
-        if s.get_info(rs.camera_info.name) == 'RGB Camera':
-            found_rgb = True
-            break
-    if not found_rgb:
-        print("The demo requires Depth camera with Color sensor")
-        exit(0)
+found_rgb = False
+for s in device.sensors:
+    if s.get_info(rs.camera_info.name) == 'RGB Camera':
+        found_rgb = True
+        break
+if not found_rgb:
+    print("The demo requires Depth camera with Color sensor")
+    exit(0)
 
-    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
 
-    if device_product_line == 'L500':
-        config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
-    else:
-        config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+if device_product_line == 'L500':
+    config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
+else:
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
-    # Start streaming
-    profile = pipeline.start(config)
+# Start streaming
+profile = pipeline.start(config)
 
-    # Create an align object
-    # rs.align allows us to perform alignment of depth frames to others frames
-    # The "align_to" is the stream type to which we plan to align depth frames.
-    align_to = rs.stream.color
-    align = rs.align(align_to)
+# Getting the depth sensor's depth scale (see rs-align example for explanation)
+depth_sensor = profile.get_device().first_depth_sensor()
+depth_scale = depth_sensor.get_depth_scale()
+print("Depth Scale is: " , depth_scale)
 
-    frames = pipeline.wait_for_frames()
-    # Align the depth frame to color frame
-    aligned_frames = align.process(frames)
-    color_frame = frames.get_color_frame()
-    depth_frame = frames.get_depth_frame()
+# Create an align object
+# rs.align allows us to perform alignment of depth frames to others frames
+# The "align_to" is the stream type to which we plan to align depth frames.
+align_to = rs.stream.color
+align = rs.align(align_to)
 
-    # Convert images to numpy arrays
-    color_image = np.asanyarray(color_frame.get_data())
+frames = pipeline.wait_for_frames()
+# Align the depth frame to color frame
+aligned_frames = align.process(frames)
+depth_frame = frames.get_depth_frame()
+color_frame = frames.get_color_frame()
 
-    face_cascade = cv2.CascadeClassifier('data/haarcascades/haarcascade_frontalface_default.xml')
+# Convert images to numpy arrays
+depth_image = np.asanyarray(depth_frame.get_data())
+color_image = np.asanyarray(color_frame.get_data())
 
-    inMiningArea = 0
+hsv = cv2.cvtColor(color_image, cv2.COLOR_BGR2HSV)
+orange_lower = np.array([0, 200, 20], np.uint8)
+orange_upper = np.array([60, 255, 255], np.uint8)
+orange_mask = cv2.inRange(hsv, orange_lower, orange_upper)
+Moments = cv2.moments(orange_mask)
+if Moments["m00"] != 0:
+    cX = int(Moments["m10"] / Moments["m00"])
+    cY = int(Moments["m01"] / Moments["m00"])
+else:
+    cX, cY = 0,0
 
-    while(1):
+
+try:
+    while True:
+
         # Wait for a coherent pair of frames: depth and color
         frames = pipeline.wait_for_frames()
+        depth_frame = frames.get_depth_frame()
         color_frame = frames.get_color_frame()
+        if not depth_frame or not color_frame:
+            continue
 
         # Convert images to numpy arrays
-        color_image = np.asanyarray(color_frame.get_data())
         depth_image = np.asanyarray(depth_frame.get_data())
+        color_image = np.asanyarray(color_frame.get_data())
 
-        hsv = cv2.cvtColor(color_image, cv2.COLOR_BGR2HSV)
+        # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
+        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
 
-        if(inMiningArea == False):
-            orange_lower = np.array([0, 200, 20], np.uint8)
-            orange_upper = np.array([60, 255, 255], np.uint8)
-            orange_mask = cv2.inRange(hsv, orange_lower, orange_upper)
-            Moments = cv2.moments(orange_mask)
-            if Moments["m00"] != 0:
-                cX = int(Moments["m10"] / Moments["m00"])
-                cY = int(Moments["m01"] / Moments["m00"])
-            else:
-                cX, cY = 0,0
-            cv2.circle(color_image, (cX, cY), 5, (0, 165, 255), -1)
+        depth_colormap_dim = depth_colormap.shape
+        color_colormap_dim = color_image.shape
 
-            distance = depth_frame.get_distance(100,100)
+        # If depth and color resolutions are different, resize color image to match depth image for display
+        if depth_colormap_dim != color_colormap_dim:
+            resized_color_image = cv2.resize(color_image, dsize=(depth_colormap_dim[1], depth_colormap_dim[0]), interpolation=cv2.INTER_AREA)
+            images = np.hstack((resized_color_image, depth_colormap))
+        else:
+            images = np.hstack((color_image, depth_colormap))
 
-        cv2.namedWindow('RobotVision', cv2.WINDOW_AUTOSIZE)
-        cv2.imshow('RobotVision', color_image) 
+        cv2.circle(color_image, (640,240), (5), (0, 0, 255), 2, 1)
+
+        distance = depth_frame.get_distance(cX,cY)
+        # Show images
+        cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
+        cv2.imshow('RealSense', images)
         cv2.waitKey(1)
 
-        if (distance > 1.5):
-                body = 7000
-                tango.setTarget(BODY, body)
-        else:
-            body = 6000
-            tango.setTarget(BODY, body)
+        if(distance < 1):
+            body += 200
+            if(body >7900):
+                   body = 7900
+            tango.setTarget(BODY,body)
+        elif(distance > 1):
+            body -= 200
+            if(body < 1510):
+                body = 1510
+            tango.setTarget(BODY,body)
 
-        if (cX > 390):
-            motors = 5200
-            tango.setTarget(MOTORS, motors)
-        elif (cX < 250):
-            motors = 6800
-            tango.setTarget(MOTORS, motors)
-        else:
-            motors = 6000
-            tango.setTarget(MOTORS, motors)
+            
 
-        print(distance)
+
+
+finally:
+
     # Stop streaming
     pipeline.stop()
-
-orient_finish()

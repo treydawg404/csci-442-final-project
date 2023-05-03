a431a382b36b271500099ee1413909a17f0fc50a
Trey <treyton.exe@gmail.com>
2023-05-03 01:34:28 -0600

    ""

--- final.py
+++ final.py
@@ -135,7 +135,7 @@ def orientation_cone():
                         tango.setTarget(MOTORS,motors)
                         body = 5200 
                         tango.setTarget(BODY,body)
-                        time.sleep(0.5)
+                        time.sleep(0.3)
                         body = 6000 
                         tango.setTarget(BODY,body)
 

f9e473f90bec9f0f65dedc34cf8a4588d933e75f
Trey <treyton.exe@gmail.com>
2023-05-03 09:35:19 -0600

    ""

--- final.py
+++ final.py
@@ -506,8 +506,8 @@ def goal_find(savedColor):
     green_lower = np.array([150, 220, 40], np.uint8)
     green_upper = np.array([180, 255,100], np.uint8)
 
-    pink_lower = np.array([50, 10, 50], np.uint8)
-    pink_upper = np.array([255, 150, 255], np.uint8)
+    pink_lower = np.array([170, 10, 110], np.uint8)
+    pink_upper = np.array([255, 75, 200], np.uint8)
 
     count = 0
     headTilt = 4800

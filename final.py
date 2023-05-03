0fc8f55ef2e4209204b96212d8ba04fad2042c25
Trey <treyton.exe@gmail.com>
2023-05-03 11:44:44 -0600

    ""

--- final.py
+++ final.py
@@ -505,7 +505,7 @@ def goal_find(savedColor):
     goalColor = savedColor
 
 
-    yellow_lower = np.array([100, 100, 0], np.uint8)
+    yellow_lower = np.array([50, 50, 0], np.uint8)
     yellow_upper = np.array([255, 255, 30], np.uint8)
 
     green_lower = np.array([40, 140, 40], np.uint8)

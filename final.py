260e1b3d2a8064abf576dfb1c802277c0f4026f9
Trey <treyton.exe@gmail.com>
2023-05-03 11:20:14 -0600

    ""

--- final.py
+++ final.py
@@ -346,8 +346,8 @@ def color_find():
     #yellow_lower = np.array([120, 150, 150], np.uint8)
     #yellow_upper = np.array([140, 255, 200], np.uint8)
 
-    green_lower = np.array([150, 220, 40], np.uint8)
-    green_upper = np.array([180, 255,100], np.uint8)
+    green_lower = np.array([40, 140, 40], np.uint8)
+    green_upper = np.array([120, 255,120], np.uint8)
 
     pink_lower = np.array([125, 0, 125], np.uint8)
     pink_upper = np.array([255, 100, 255], np.uint8)
